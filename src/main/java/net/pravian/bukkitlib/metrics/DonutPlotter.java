e4970c346a06bee28c6ff749dd4321588168499c
unknown <jerom.sar@hotmail.com>
2014-01-28 20:04:01 +0100

    Added documentation for new plotters

--- src/main/java/net/pravian/bukkitlib/metrics/DonutPlotter.java
+++ src/main/java/net/pravian/bukkitlib/metrics/DonutPlotter.java
@@ -1,9 +1,20 @@
 package net.pravian.bukkitlib.metrics;
 
+/**
+ * Represents a plotter with special data for a donut-type graph.
+ */
 public abstract class DonutPlotter extends Plotter {
-
+    
     private String majorName, minorName;
 
+    /**
+     * Creates a new DonutPlotter instance with the specified major and minor names.
+     * 
+     * <p>The major name indicates the center area of the donut graph whilst the minor name indicates the edges.</p>
+     * 
+     * @param major The donut filling (usually air).
+     * @param minor The donut crust (usually dough with icing).
+     */
     public DonutPlotter(String major, String minor) {
         super(major + "~=~" + minor);
     }
--- src/main/java/net/pravian/bukkitlib/metrics/FixedPlotter.java
+++ src/main/java/net/pravian/bukkitlib/metrics/FixedPlotter.java
@@ -1,13 +1,27 @@
 package net.pravian.bukkitlib.metrics;
 
+/**
+ * Represents a plotter with a fixed value.
+ */
 public class FixedPlotter extends Plotter {
 
     private final int value;
 
+    /**
+     * Creates a new FixedPlotter instance with a value of 1.
+     * 
+     * @param name The plotter name.
+     */
     public FixedPlotter(final String name) {
         this(name, 1);
     }
 
+    /**
+     * Creates a new FixedPlotter instance with the specified value.
+     * 
+     * @param name The plotter name.
+     * @param value The plotter value.
+     */
     public FixedPlotter(final String name, final int value) {
         super(name);
         this.value = value;
